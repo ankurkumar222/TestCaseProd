668c7131093b846a2d2172c0586c943464b470aa
ankurkumar222 <ankurkumar@ankurs-MacBook-Air.local>
2016-11-19 09:25:15 +0530

    commit

---
+++ src/assignments/questions/linkedlist/LinkedListNode.java
@@ -0,0 +1,20 @@
+package assignments.questions.linkedlist;
+
+public class LinkedListNode<T> {
+	public T data;
+	public LinkedListNode<T> next;
+
+	public LinkedListNode(T data) {
+		this.setData(data);
+		this.next = null;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {		
+		this.data = data;
+	}
+
+}
